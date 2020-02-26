b91a8ce732d84bc9a4afd2f10238db5549123570
EC2 Default User <ec2-user@ip-172-31-42-235.ec2.internal>
2020-02-26 11:55:47 +0000

    initial version

---
+++ v1/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/tic-tac-toe", function(req, res){
+    res.render("tic");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The server has started");
+});
